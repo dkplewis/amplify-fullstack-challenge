0ffa5bf7b192d9d0928a0264d15dd899325f679f
gardin-davidl <d.lewis@gardin.co.uk>
2024-05-24 10:19:13 +0100

    Reapplying Auth category

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
-//import { auth } from './auth/resource';
+import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 
@@ -7,7 +7,7 @@ import { storage } from './storage/resource';
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
-  //auth,
+  auth,
   data,
   storage
 });
